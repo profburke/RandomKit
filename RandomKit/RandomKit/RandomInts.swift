b53ed050f4243d376e14561fe5a06ec1a82983d1
Matthew M. Burke <matthew@logicnets.com>
2015-04-23 10:32:53 -0400

    Some refactoring and some additions

---
+++ RandomKit/RandomKit/RandomInts.swift
@@ -0,0 +1,330 @@
+//
+//  RandomInts.swift
+//  RandomKit
+//
+//  Created by Matthew Burke on 4/22/15.
+//  Copyright (c) 2015 BlueDino Software. All rights reserved.
+//
+
+import Foundation
+
+
+// TODO: It would be nice to simplify this with generics, but I'm not sure that's possible.
+
+
+// MARK: Int64, UInt64
+
+
+public func randomInt64() -> Int64
+{
+  return Int64.max
+}
+
+
+
+
+public func randomInt64(range: Range<Int64>) -> Int64
+{
+  return Int64.max
+}
+
+
+
+
+public func randomUInt64() -> UInt64
+{
+  return UInt64.max
+}
+
+
+
+
+public func randomUInt64(upper: UInt64) -> UInt64
+{
+  return UInt64.max
+}
+
+
+
+
+public func randomUInt64(range: Range<UInt64>) -> UInt64
+{
+  return UInt64.max
+}
+
+
+
+
+// MARK: Int32, UInt32
+
+
+public func randomInt32() -> Int32
+{
+  let r = arc4random()
+  return Int32(r - UInt32.max/2) // FIXME: this isn't correct
+}
+
+
+
+
+public func randomInt32(range: Range<Int32>) -> Int32
+{
+  let upper = UInt32(range.endIndex - range.startIndex)
+  let r = randomUInt32(upper)
+  return Int32.max
+}
+
+
+
+
+/**
+
+  Generates a random UInt32.
+
+  :returns: A random UInt32.
+
+*/
+public func randomUInt32() -> UInt32
+{
+  return arc4random()
+}
+
+
+
+
+/**
+
+  Generates a random UInt32 in the range 0..<upper.
+
+  :param: upper The upper bound for the returned random number.
+  :returns: a random UInt32 in the range 0..<upper.
+
+*/
+public func randomUInt32(upper: UInt32) -> UInt32
+{
+  return arc4random_uniform(upper)
+}
+
+
+
+
+/**
+
+  Generates a random UInt32 in the given range.
+
+  :param: range The range from which to generate the random number.
+  :returns: a random UInt32 from the given range.
+
+*/
+public func randomUInt32(range: Range<UInt32>) -> UInt32
+{
+  let upper = range.endIndex - range.startIndex
+  let r = arc4random_uniform(upper)
+  return range.startIndex + r
+}
+
+
+
+
+// MARK: Int16, UInt16
+
+
+public func randomInt16() -> Int16
+{
+  let r = randomInt32(Int32(Int16.min)..<(Int32(Int16.max) + 1))
+  return Int16(r)
+}
+
+
+
+
+public func randomInt16(range: Range<Int16>) -> Int16
+{
+  let r = randomInt32(Int32(range.startIndex)..<Int32(range.endIndex))
+  return Int16(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt16.
+
+:returns: A random UInt16.
+
+*/
+public func randomUInt16() -> UInt16
+{
+  let r = randomUInt32(UInt32(UInt16.max) + 1)
+  return UInt16(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt16 in the given range.
+
+:param: range The range from which to generate the random number.
+:returns: a random UInt16 from the given range.
+
+*/
+public func randomUInt16(range: Range<UInt16>) -> UInt16
+{
+  let upper = range.endIndex - range.startIndex
+  let r = arc4random_uniform(UInt32(upper))
+  return range.startIndex + UInt16(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt16 in the range 0..<upper.
+
+:param: upper The upper bound for the returned random number.
+:returns: a random UInt16 in the range 0..<upper.
+
+*/
+public func randomUInt16(upper: UInt16) -> UInt16
+{
+  return randomUInt16(0..<upper)
+}
+
+
+
+
+// MARK: Int8, UInt8
+
+
+public func randomInt8() -> Int8
+{
+  let r = randomInt32(Int32(Int16.min)..<(Int32(Int16.max) + 1))
+  return Int8(r)
+}
+
+
+
+
+public func randomInt8(range: Range<Int8>) -> Int8
+{
+  let r = randomInt32(Int32(range.startIndex)..<Int32(range.endIndex))
+  return Int8(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt8.
+
+:returns: A random UInt8.
+
+*/
+public func randomUInt8() -> UInt8
+{
+  let r = randomUInt32(UInt32(UInt8.max) + 1)
+  return UInt8(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt8 in the given range.
+
+:param: range The range from which to generate the random number.
+:returns: a random UInt8 from the given range.
+
+*/
+public func randomUInt8(range: Range<UInt8>) -> UInt8
+{
+  let upper = range.endIndex - range.startIndex
+  let r = arc4random_uniform(UInt32(upper))
+  return range.startIndex + UInt8(r)
+}
+
+
+
+
+/**
+
+Generates a random UInt8 in the range 0..<upper.
+
+:param: upper The upper bound for the returned random number.
+:returns: a random UInt8 in the range 0..<upper.
+
+*/
+public func randomUInt8(upper: UInt8) -> UInt8
+{
+  return randomUInt8(0..<upper)
+}
+
+
+
+
+// MARK: Int, UInt
+
+
+public func randomInt() -> Int
+{
+  if is64BitPlatform() {
+    return Int(randomInt64())
+  } else {
+    return Int(randomInt32())
+  }
+}
+
+
+
+
+public func randomInt(range: Range<Int>) -> Int
+{
+  if is64BitPlatform() {
+    return Int(randomInt64(Int64(range.startIndex)..<Int64(range.endIndex)))
+  } else {
+    return Int(randomInt32(Int32(range.startIndex)..<Int32(range.endIndex)))
+  }
+}
+
+
+
+
+public func randomUInt() -> UInt
+{
+  if is64BitPlatform() {
+    return UInt(randomUInt64())
+  } else {
+    return UInt(randomUInt32())
+  }
+}
+
+
+
+
+public func randomUInt(upper: UInt) -> UInt
+{
+  if is64BitPlatform() {
+    return UInt(randomUInt64(UInt64(upper)))
+  } else {
+    return UInt(randomUInt32(UInt32(upper)))
+  }
+}
+
+
+
+
+public func randomUInt(range: Range<UInt>) -> UInt
+{
+  if is64BitPlatform() {
+    return UInt(randomUInt64(UInt64(range.startIndex)..<UInt64(range.endIndex)))
+  } else {
+    return UInt(randomUInt32(UInt32(range.startIndex)..<UInt32(range.endIndex)))
+  }
+}
+
---
+++ RandomKit/RandomKit/Utilities.swift
@@ -0,0 +1,42 @@
+//
+//  Utilities.swift
+//  RandomKit
+//
+//  Created by Matthew Burke on 4/22/15.
+//  Copyright (c) 2015 BlueDino Software. All rights reserved.
+//
+
+import Foundation
+
+
+/**
+
+Determine whether platform is 64 bits.
+
+:returns: True if the platform is 64 bits, false otherwise.
+
+*/
+public func is64BitPlatform() -> Bool
+{
+  return sizeof(Int) == sizeof(Int64)
+}
+
+
+
+
+/**
+
+Map a value to the given range.
+
+:param: value The value to transform.
+:param: range The range to which the given value should be mapped.
+:returns: The mapped value.
+
+*/
+public func linearTransform(value: Double, range: Range<Int>) -> Double
+{
+  return Double(range.startIndex) + Double(range.endIndex - range.startIndex) * value
+}
+
+
+
