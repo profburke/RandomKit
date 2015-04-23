b31719e68ad496474f70050af2d0af270702c7d1
Matthew M. Burke <matthew@logicnets.com>
2015-04-23 11:18:08 -0400

    more rearranging

---
+++ RandomKit/RandomKit/RandomReals.swift
@@ -0,0 +1,27 @@
+//
+//  RandomReals.swift
+//  RandomKit
+//
+//  Created by Matthew Burke on 4/23/15.
+//  Copyright (c) 2015 BlueDino Software. All rights reserved.
+//
+
+import Foundation
+
+
+
+/**
+
+Generates a uniformly-distributed random double.
+
+The default range is 0..<1.
+
+:param: range The range from which the random value should be chosen.
+:returns: a uniformly-distributed random double.
+
+*/
+public func uniform(_ range: Range<Int> = 0..<1) -> Double
+{
+  let r = Double(arc4random())/Double(UInt32.max)
+  return linearTransform(r, range)
+}
